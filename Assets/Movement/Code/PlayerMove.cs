da301c680f9d430e803fab07520669b010148106
folakemi10 <71245496+folakemi10@users.noreply.github.com>
2024-02-09 10:11:21 -0600

    basic movement
    some movement

---
+++ Assets/Movement/Code/PlayerMove.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float moveSpeed = 3;
+    public float leftRightSpeed = 4;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed, Space.World);
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            if(this.gameObject.transform.position.x> Boundary.leftSide) {
+                transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed);
+            }
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            if (this.gameObject.transform.position.x > Boundary.rightSide)
+            {
+                transform.Translate(Vector3.left* Time.deltaTime * leftRightSpeed);
+            }
+        }
+    }
+}
