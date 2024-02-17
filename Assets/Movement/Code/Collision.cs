3ea681c2754545df88a93ff6068fb45b1d91c98f
abattin <50970328+abattin@users.noreply.github.com>
2024-02-16 21:08:29 -0600

    Added collision to obstacle

---
+++ Assets/Movement/Code/Collision.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Collision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(UnityEngine.Collision collision)
+    {
+        Debug.Log("We hit something");
+        Debug.Log(collision.gameObject.name);
+        Debug.Log(collision.gameObject.tag);
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log("We hit an obstacle");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        
+}
+}
