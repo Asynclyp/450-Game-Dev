ef5bf56ddd2a1a1b5c4ffb6916f4e2a3647d4133
Yuanpeng Li <l.yuanpeng@wustl.edu>
2024-02-20 12:00:53 -0600

    Fixed level overlap issue
    There's no gap in the initial scene now, and the first dynamical level will be generated at z=200 instead of 50.

--- Assets/Movement/Code/GenerateLevel.cs
+++ Assets/Movement/Code/GenerateLevel.cs
@@ -4,29 +4,34 @@ using UnityEngine;
 
 public class GenerateLevel : MonoBehaviour
 {
-
     public GameObject[] section;
-    public int zPos = 50;
+    public int zPos;
     public bool creatingSection = false;
     public int secNum;
+
+
+    void Start()
+    {
+        zPos = 200;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if(creatingSection == false)
+        if (creatingSection == false)
         {
             creatingSection = true;
             StartCoroutine(GenerateSection());
-        }
-        
+        }   
     }
 
     IEnumerator GenerateSection()
     {
+        Debug.Log("New scene at " + zPos);
         secNum = Random.Range(0, 3);
         Instantiate(section[secNum], new Vector3(0,0, zPos), Quaternion.identity);
         zPos += 50; 
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(10);
         creatingSection = false;
     }
 }
