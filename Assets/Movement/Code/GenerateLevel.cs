de2d78fe73b4724342cc87bf35704c66966fd529
yc3333 <yc017@bucknell.edu>
2024-02-20 17:18:21 -0600

    limit level amount

--- Assets/Movement/Code/GenerateLevel.cs
+++ Assets/Movement/Code/GenerateLevel.cs
@@ -8,30 +8,54 @@ public class GenerateLevel : MonoBehaviour
     public int zPos;
     public bool creatingSection = false;
     public int secNum;
+    public Transform player;
+
+    private List<GameObject> activeScenes = new List<GameObject>();
 
 
     void Start()
     {
         zPos = 200;
+        for (int i =0; i< 7; i++)
+        {
+            secNum = Random.Range(0, 3);
+            GameObject newScene = Instantiate(section[secNum], new Vector3(0, 0, zPos), Quaternion.identity);
+            activeScenes.Add(newScene);
+            zPos += 50;
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (creatingSection == false)
+        if (player.position.z > (zPos-500))
         {
-            creatingSection = true;
-            StartCoroutine(GenerateSection());
-        }   
+            //creatingSection = true;
+            GenerateSection();
+        }
     }
 
-    IEnumerator GenerateSection()
+    void GenerateSection()
     {
         Debug.Log("New scene at " + zPos);
         secNum = Random.Range(0, 3);
-        Instantiate(section[secNum], new Vector3(0,0, zPos), Quaternion.identity);
-        zPos += 50; 
-        yield return new WaitForSeconds(10);
-        creatingSection = false;
+        GameObject newScene = Instantiate(section[secNum], new Vector3(0, 0, zPos), Quaternion.identity);
+        zPos += 50;
+        //yield return new WaitForSeconds(10);
+        //creatingSection = false;
+        activeScenes.Add(newScene);
+        if (activeScenes.Count > 10)
+        {
+            GameObject sceneToRemove = activeScenes[0];
+            activeScenes.RemoveAt(0);
+            Destroy(sceneToRemove);
+            Debug.Log("Destroy one scene" );
+
+        }
     }
+
+    
 }
+
+
